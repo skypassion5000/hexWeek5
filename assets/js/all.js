bc642746bd570d37ca0b287e19d78d339a4b83be
hsiang hsu <skypassion5000@gmail.com>
2023-02-03 16:44:32 +0800

    Update 2023-02-03T08:44:29.901Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,9 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
+});
 //# sourceMappingURL=all.js.map
